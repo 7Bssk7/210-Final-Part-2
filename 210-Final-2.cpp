074b734655515260c09ad2c10a314fa2e507a097
7Bssk7 <arhip.fin3@gmail.com>
2025-12-11 19:51:07 -0800

    Update

--- 210-Final-2.cpp
+++ 210-Final-2.cpp
@@ -6,7 +6,7 @@
 #include <ctime>
 using namespace std;
 
-const int LINE_SIZE = 3;
+const int LINE_SIZE = 3, SIMULATION = 10;
 
 class LinkedList{
     public: 
@@ -14,6 +14,7 @@ class LinkedList{
 
         void push_back(string n, string d) {
             Node* newNode = new Node(n, d);
+            cout << "    New customer joins: " << n << ", Ordered: " << d << endl;
             if (!tail) {
                 head = tail = newNode;
             } else {
@@ -75,7 +76,7 @@ string randomDrink(const vector<string>& );
 bool chance(int p);
 
 int main(){
-    vector<string> names {"Alice","Bob","Charlie","Diana","Ethan","Fiona"};
+    vector<string> names {"Alice","Bob","Charlie","Diana","Ethan","Fiona","Ben","Nick","Bryan","Finn","Arkhip"};
     vector<string> drinks = {"Latte","Espresso","Mocha","Tea","Cappuccino","Americano"};
 
     LinkedList line; 
@@ -87,7 +88,10 @@ int main(){
         line.push_back(randomName(names),randomDrink(drinks));
     }
 
+    cout << " Coffe Booth Line simulation:" << endl;
+    cout << " Initial Line: " << endl;
     line.print();
+
 
     return 0;
 }
