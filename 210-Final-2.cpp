982e610719a273fac312b1205feb31be6740a550
7Bssk7 <arhip.fin3@gmail.com>
2025-12-11 19:08:38 -0800

    Update

--- 210-Final-2.cpp
+++ 210-Final-2.cpp
@@ -1,9 +1,13 @@
 // COMSC-210 | Final 2 | Arkhip Finski
 // IDE used: Visual Studio
 #include <iostream>
+#include <vector>
+#include <string>
+#include <ctime>
 using namespace std;
 
 int main(){
+    vector<string> names {"Alice","Bob","Charlie","Diana","Ethan","Fiona"};
 
     return 0;
 }
